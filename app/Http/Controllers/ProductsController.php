dea84f86a0113d5e7328968e7fdb70d0c38d6d41
Daniel An <cruzalexd1@gmail.com>
2024-06-06 11:48:53 -0600

    :sparkles:feat(ProductsController):add api funcionalities

---
+++ app/Http/Controllers/ProductsController.php
@@ -0,0 +1,179 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Products;
+use Illuminate\Support\Facades\Validator;
+
+class ProductsController extends Controller
+{
+    public function getAllProductsEnabled()
+    {
+
+        $products = Products::where('disabled', false)->get();
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Got alll enabled products',
+            'data' => $products
+        ], 200);
+
+    }
+
+    public function getAllProducts()
+    {
+        $products = Products::all();
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Got all products',
+            'data' => $products
+        ], 200);
+    }
+
+    public function getSingleProduct($id)
+    {
+
+        $products = Products::find($id);
+
+        if (!$products) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Product was not found',
+                'data' => 'No data'
+            ], 404);
+        }
+
+        if ($products->disabled) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Product has been eliminated',
+                'data' => 'No data'
+            ], 400);
+        }
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Product with id ' . $id . ' was found',
+            'data' => $products
+        ]);
+
+    }
+
+    public function addProducts(Request $request)
+    {
+        $repeated_product = Products::where('nombre', $request->nombre)->first();
+        if ($repeated_product) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Product already exists on database',
+                'data' => 'No data'
+            ]);
+        }
+
+        $validator = Validator::make($request->all(), [
+            'categoria' => 'required|string',
+            'nombre' => 'required|string',
+            'marca' => 'required|string',
+            'precio' => 'required|string',
+            'stock' => 'required|numeric'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Validation error',
+                'data' => $validator->errors()
+            ], 409);
+        }
+
+        $products = Products::create($request->all());
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Product has been added to the database',
+            'data' => $products
+        ], 200);
+
+    }
+
+    public function updateProducts(Request $request, $id)
+    {
+        $product = Products::find($id);
+        if (!$product) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Product with id ' . $id . ' does not exist',
+                'data' => 'No data'
+            ], 404);
+        }
+
+        $product->update($request->all());
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Product with id ' . $id . ' has been updated',
+            'data' => $product
+        ], 200);
+    }
+
+    public function disableProduct($id)
+    {
+
+        $product = Products::find($id);
+
+        if (!$product) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Product with id ' . $id . ' does not exist',
+                'data' => 'No data'
+            ], 404);
+        }
+
+        if ($product->disabled) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Product with id ' . $id . ' has been already disabled',
+                'data' => 'No data'
+            ], 409);
+        }
+
+        $product->update(['disabled' => true]);
+        return response()->json([
+            'status' => true,
+            'message' => 'Product with id ' . $id . ' has been disabled',
+            'data' => $product
+        ], 200);
+
+    }
+
+    public function enableProduct($id)
+    {
+        $product = Products::find($id);
+
+        if (!$product) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Product with id ' . $id . ' does not exist',
+                'data' => 'No data'
+            ], 404);
+        }
+
+        if (!($product->disabled)) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Product with id ' . $id . ' is already enabled',
+                'data' => $product
+            ], 409);
+        }
+
+        $product->update(['disabled' => false]);
+        return response()->json([
+            'status' => true,
+            'message' => 'Product with id ' . $id . ' has been enabled',
+            'data' => $product
+        ], 200);
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthenticationController;
+use App\Http\Controllers\ProductsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,19 @@ Route::group([
 ], function ($route) {
     Route::post('/register', [AuthenticationController::class, 'register']);
     Route::post('/login', [AuthenticationController::class, 'login']);
+    Route::get('/user', [AuthenticationController::class, 'getUser']);
 });
 
-Route::get('/user', [AuthenticationController::class, 'getUser']);
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'productos'
+], function ($route) {
+    Route::get('/all-enabled', [ProductsController::class, 'getAllProductsEnabled']);
+    Route::get('/all', [ProductsController::class, 'getAllProducts']);
+    Route::get('/{id}', [ProductsController::class, 'getSingleProduct']);
+    Route::post('/add', [ProductsController::class, 'addProducts']);
+    Route::patch('/update/{id}', [ProductsController::class, 'updateProduct']);
+    Route::post('/disable/{id}', [ProductsController::class, 'disableProduct']);
+    Route::post('/enable/{id}', [ProductsController::class, 'enableProduct']);
+});
+
